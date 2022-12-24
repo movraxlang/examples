49d243345fcfff74b7b549afd725a08d52426c6b
lateralusd <xdaemonx@protonmail.ch>
2022-12-24 11:21:09 +0100

    add c test file

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int test_func(void) {
+	printf("i am test_func\n");
+	return 0;
+}
+
+int main(int argc, char ** argv) {
+	printf("main called\n");
+	int ret = test_func();
+	printf("i have got %d\n", ret);
+	return 0;
+}
